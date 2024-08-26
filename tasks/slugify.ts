5df6c5bf868c7849ebf3a0f8401dc854cdc6b771
control <such_shifter280@pilina.email>
2024-08-26 14:24:27 +0200

    update tasks even simpler

--- tasks/slugify.ts
+++ tasks/slugify.ts
@@ -1,17 +1,4 @@
-import { stdin } from "node:process"
 import { slugify } from "../src/utils/common-utils"
-let input
-if (stdin.isTTY) {
-  input = process.argv[2]
-  if (!input) {
-    console.error("Please provide an argument or pipe input")
-    process.exit(1)
-  }
-} else {
-  const chunks = []
-  for await (const chunk of stdin) {
-    chunks.push(chunk)
-  }
-  input = Buffer.concat(chunks).toString().trim()
-}
-console.log(slugify(input))
+const str =
+  (await Bun.readableStreamToText(Bun.stdin.stream()))?.trim() || Bun.argv[2]
+console.log(slugify(str))
